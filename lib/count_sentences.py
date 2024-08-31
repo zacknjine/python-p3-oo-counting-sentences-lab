a44130d5aa588387266784fd1ddf12c017164af9
Zack Njine Wachira <zackwachira45@gmail.com>
2024-09-01 00:03:33 +0300

    Initial commit

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+
+  def __init__(self, value = ""):
+    self._value = value
+    
+  def get_value(self):
+    return self._value
+
+  def set_value(self, stringVal):
+    if (type(stringVal) == str):
+      self._value = stringVal
+    else:
+      print("The value must be a string.")
+
+  value = property(get_value, set_value)
+
+  def is_sentence(self):
+    return self._value.endswith(".")
+
+  def is_question(self):
+    return self._value.endswith("?")
+
+  def is_exclamation(self):
+    return self._value.endswith("!")
+
+  def count_sentences(self):
+    value = self.value
+    for punc in ['!','?']:
+        value = value.replace(punc, '.')
+    
+    sentences = [s for s in value.split('.') if s]
+    
+    return len(sentences)
